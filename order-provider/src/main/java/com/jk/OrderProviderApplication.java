c068ab34819798a9614ceba205e42b3c24ebd396
yang-yaxin <1952381578@qq.com>
2020-11-13 11:23:08 +0800

    yyx

--- order-provider/src/main/java/com/jk/OrderProviderApplication.java
+++ order-provider/src/main/java/com/jk/OrderProviderApplication.java
@@ -2,9 +2,11 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableEurekaClient
 public class OrderProviderApplication {
 
